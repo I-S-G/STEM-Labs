4cf753bad502f77b538ee9bad10c652ad5b379bc
I-S-G <emaangrg@gmail.com>
2026-05-12 15:48:43 +1000

    SCRUM-11 implemented frontend for home page, updated icons and styling for tab bar

--- app/(activity1)/activity1.tsx
+++ app/(activity1)/activity1.tsx
@@ -3,7 +3,7 @@ import { View, Text } from "react-native"
 export default function Activity() {
     return (
         <View>
-            <Text> This is Activity </Text>
+            <Text> This is Activity 1 </Text>
         </View>
     )
 }
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,17 +1,98 @@
-import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { Href, Link } from "expo-router";
+import { Text, View, FlatList, StyleSheet } from "react-native";
+
+const Activities = [
+  {
+    title: "Activity 1",
+    url: "/activity1",
+    description: "Description about this activity",
+  },
+  {
+    title: "Activity 2",
+    url: "/activity2",
+    description: "Description about this activity",
+  },
+  {
+    title: "Activity 3",
+    url: "/activity3",
+    description: "Description about this activity",
+  },
+  {
+    title: "Activity 4",
+    url: "/activity4",
+    description: "Description about this activity",
+  },
+  {
+    title: "Activity 5",
+    url: "/activity5",
+    description: "Description about this activity",
+  },
+  {
+    title: "Activity 6",
+    url: "/activity6",
+    description: "Description about this activity",
+  },
+  {
+    title: "Activity 7",
+    url: "/activity7",
+    description: "Description about this activity",
+  },
+]
+
+type ActivityProps = {title: string, url: Href, description: string}
+
+const Activity = (activity: ActivityProps) => {
+  const {title, url, description } = activity;
+  return (
+    <Link href={url} style= {styles.activity}>
+      <View style= {{width: "100%"}}>
+        <Text style={styles.activityTitle}> {title} </Text>
+        <Text> {description} </Text>
+      </View>
+    </Link>
+
+  )
+}
 
 export default function Home() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text> Home </Text>
-      <Link href= "/activity"> Activity 1 </Link>
+    <View style= {styles.container}>
+      <Text style={styles.title}> Choose Activity </Text>
+      <FlatList
+        data={Activities}
+        renderItem={({item}) => <Activity title= {item.title} url= {item.url as Href} description={item.description} />}
+        keyExtractor={item => item.title}
+        contentContainerStyle= {{
+          paddingBottom: 150,
+        }}
+       />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#404A52',
+  },
+  title: {
+    fontSize: 40,
+    textAlign: "center",
+    color: "white",
+    marginBottom: 10,
+    marginTop: 70,
+  },
+  activity: {
+    backgroundColor: "#99b9e2",
+    marginHorizontal: 50,
+    marginVertical: 10,
+    paddingVertical: 15,
+    paddingHorizontal: 5
+
+  },
+  activityTitle: {
+    color: "black",
+    fontWeight: "bold",
+    fontSize: 16,
+  },
+})
+
