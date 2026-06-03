b5ea23078949144c20f66936316b9dab7e582408
I-S-G <emaangrg@gmail.com>
2026-06-03 21:25:46 +1000

    env variables for firebaseconfig

--- utils/firebase/firebase.ts
+++ utils/firebase/firebase.ts
@@ -2,19 +2,16 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDsYgqpF0Y832b3H2xOqR55ddQ-Um94r74",
-  authDomain: "stem-labs-414fc.firebaseapp.com",
-  projectId: "stem-labs-414fc",
-  storageBucket: "stem-labs-414fc.firebasestorage.app",
-  messagingSenderId: "281635045001",
-  appId: "1:281635045001:web:df7771928f8fa1ab19d058",
-  measurementId: "G-8NSZHBKB95",
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
